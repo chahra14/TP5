c0cd3d70ed60daed17f51e134294b3e7e5ad098d
chahra14 <122919979+chahra14@users.noreply.github.com>
2023-11-22 13:41:03 -0500

    Changement AdminUnit dans Drone pour AbsUnit

--- DroneRescue/Drone.cpp
+++ DroneRescue/Drone.cpp
@@ -2,21 +2,20 @@
 //  Drone.cpp
 //  Implementation of the Class Drone
 //  Created on:      2023-10-30
-//  Original author: Eric Germain basé sur les travaux de Francois Guibault
+//  Original author: Eric Germain basÃ© sur les travaux de Francois Guibault
 ///////////////////////////////////////////////////////////
 
 #include "Drone.h"
 #include <iostream>
 
 Drone::Drone(std::string name, std::string escadron, std::string division)
-	: AdminUnit(name), m_id(rand())
-{
+	: AbsUnit(name), m_id(rand()) { 
 	m_properties.insert(std::pair<std::string, std::string>("division", division));
 	m_properties.insert(std::pair<std::string, std::string>("escadron", escadron));
 }
 
 Drone::Drone(std::string name, AbsUnit* parent)
-	: AdminUnit(name, parent), m_id(rand())
+	: AbsUnit(name, parent), m_id(rand())  
 {
 	m_properties.insert(std::pair<std::string, std::string>("division", parent->getProperties().at("division")));
 	m_properties.insert(std::pair<std::string, std::string>("escadron", parent->getName()));
@@ -24,19 +23,19 @@ Drone::Drone(std::string name, AbsUnit* parent)
 
 Drone* Drone::clone() const
 {
-	// À compléter}
+	// Ã€ complÃ©ter}
 	return new Drone(*this);
 }
 
 unsigned int Drone::getId(void) const
 {
-	// À compléter
+	// Ã€ complÃ©ter
 	return (m_id);
 }
 
 std::ostream& Drone::printToStream(std::ostream& o) const
 {
-	// À compléter
+	// Ã€ complÃ©ter
 	
 	++m_indent;
 	o << getName() << " [" << getId() << "] {" << getFullPath() << "}" << std::endl;
